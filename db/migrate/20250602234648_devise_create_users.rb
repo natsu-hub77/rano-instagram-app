e51765aa2f82e6164efa5d9874a5d9c6e8a95190
らの <>
2025-06-04 09:28:09 +0900

    アカウント名をuniqueにする記述を追加

--- db/migrate/20250602234648_devise_create_users.rb
+++ db/migrate/20250602234648_devise_create_users.rb
@@ -7,6 +7,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.2]
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
       t.string :account_name,       null: false, default: ""
+      t.index :account_name,        unique: true
 
       ## Recoverable
       t.string   :reset_password_token
--- db/schema.rb
+++ db/schema.rb
@@ -23,6 +23,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_06_02_234648) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["account_name"], name: "index_users_on_account_name", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
