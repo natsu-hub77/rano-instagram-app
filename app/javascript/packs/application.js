e8b80d70b0d4a9575b01c14c267650aa0235e848
らの <>
2025-07-31 19:39:30 +0900

    画像アップロードの実装を「profile.js」に分割

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,43 +9,8 @@ axios.defaults.headers.common['X-CSRF-Token'] = Rails.csrfToken()
 import * as ActiveStorage from "@rails/activestorage"
 ActiveStorage.start()
 
-document.addEventListener('DOMContentLoaded', () => {
-  const avatarUpload = document.getElementById('avatar-upload');
-  const avatarInput = document.getElementById('avatar-input');
-  const form = document.getElementById('profile-form');
-  if (!avatarInput || !form) return;
 
-  avatarUpload.addEventListener('click', () => {
-    avatarInput.click();
-  });
-
-  avatarInput.addEventListener('change', async () => {
-    const formData = new FormData(form);
-
-    try {
-      const response = await fetch(form.action, {
-        method: 'PATCH',
-        headers: {
-          'X-CSRF-Token': document.querySelector('meta[name="csrf-token"]').content
-        },
-        body: formData
-      });
-
-      if (response.ok) {
-        const data = await response.json();
-        if (data.avatar_url) {
-          avatarUpload.src = data.avatar_url;
-        }
-      } else {
-        console.error('アップロード失敗', await response.text());
-        alert('画像のアップロードに失敗しました');
-      }
-    } catch (error) {
-      console.error('通信エラー:', error);
-      alert('通信エラーが発生しました');
-    }
-  });
-});
+import "./profile"
 
 // document.addEventListener('DOMContentLoaded', () => {
 //   const avatarUpload = document.getElementById('avatar-upload');
