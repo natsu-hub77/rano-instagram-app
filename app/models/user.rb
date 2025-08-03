5f36f5903ce0ef7f2e42fef700449758e78689ef
らの <>
2025-08-03 14:20:10 +0900

    comment モデルを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -26,6 +26,7 @@ class User < ApplicationRecord
 
   has_many :posts, dependent: :destroy
   has_many :likes, dependent: :destroy
+  has_many :comments, dependent: :destroy
   has_one :profile, dependent: :destroy
 
   validates :account_name, presence: true, uniqueness: true
