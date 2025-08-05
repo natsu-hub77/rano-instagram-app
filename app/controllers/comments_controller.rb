5a4683b8c505ec8c842b66a14217d419728d6788
らの <>
2025-08-05 19:57:16 +0900

    createアクションの追加

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,4 +7,21 @@ class CommentsController < ApplicationController
     @comment = @post.comments.build
   end
 
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.build(comment_params)
+    @comment.user = current_user
+    if @comment.save
+      redirect_to post_comments_path(@post), notice: 'コメントを追加しました'
+    else
+      flash.now[:error] = '更新できませんでした'
+      render :index, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
+
 end
