ad6cd6a2b213f6ccbd75a6aee12996d7571ed268
らの <>
2025-08-27 08:43:07 +0900

    コメント表示のリファクタリング

--- app/javascript/packs/comment.js
+++ app/javascript/packs/comment.js
@@ -2,6 +2,27 @@ import axios from 'axios'
 import $ from 'jquery'
 import Rails from "@rails/ujs"
 
+const appendAllComment = (comment) => {
+$('.comments-container').append(
+  `     
+    <div class="comment-post">
+      <div class="comment-icon">
+        <img src="${comment.avatar_url}" alt="${comment.account_name}" />
+      </div>
+      <div class="comment-body">
+        <div class="comment-account-name">
+          <p>${comment.account_name}</p>
+        </div>
+        <div class="comment-content">
+          <p>${comment.content}</p>
+        </div>
+      </div>
+    </div>
+  `
+  )
+}
+
+
 document.addEventListener('turbo:load', () => {
   const dataset = $('#comment-index').data()
   if (!dataset) return
@@ -15,23 +36,7 @@ document.addEventListener('turbo:load', () => {
       $('.comments-container').empty()
 
       comments.forEach((comment) => {
-        $('.comments-container').append(
-        `     
-        <div class="comment-post">
-          <div class="comment-icon">
-            <img src="${comment.avatar_url}" alt="${comment.account_name}" />
-          </div>
-          <div class="comment-body">
-            <div class="comment-account-name">
-              <p>${comment.account_name}</p>
-            </div>
-            <div class="comment-content">
-              <p>${comment.content}</p>
-            </div>
-          </div>
-        </div>
-        `
-        )
+        appendAllComment(comment)
       })
     })
  
@@ -51,22 +56,7 @@ document.addEventListener('turbo:load', () => {
         })
           .then((res) => {
             const comment = res.data
-              $('.comments-container').append(
-                `     
-                <div class="comment-post">
-                  <div class="comment-icon">
-                    <img src="${comment.avatar_url}" alt="${comment.account_name}" />
-                  </div>
-                  <div class="comment-body">
-                    <div class="comment-account-name">
-                      <p>${comment.account_name}</p>
-                    </div>
-                    <div class="comment-content">
-                      <p>${comment.content}</p>
-                    </div>
-                </div>
-              </div>
-              `)
+            appendAllComment(comment)
             $('#comment_content').val('')
           })
     })
