d2e0c8b692f36366e516c7f68f3db651fe1eafc6
らの <>
2025-09-05 21:26:31 +0900

    フォロワーの関係を作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -31,11 +31,18 @@ class User < ApplicationRecord
   has_many :following_relationships, foreign_key: 'follower_id', class_name: 'Relationship', dependent: :destroy
   has_many :followings, through: :following_relationships, source: :following
 
+  has_many :follower_relationships, foreign_key: 'following_id', class_name: 'Relationship', dependent: :destroy
+  has_many :followers, through: :follower_relationships, source: :follower
+
   has_one :profile, dependent: :destroy
 
   validates :account_name, presence: true, uniqueness: true
 
   def has_liked?(post)
     likes.exists?(post_id: post.id)
+  end
+  
+  def follow!(user)
+    following_relationships.create!(following_id: user.id) 
   end
 end
