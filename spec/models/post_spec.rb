e000ca282ec2a0555e7aab0a447e5f0863ddbcf1
らの <>
2025-09-28 08:31:02 +0900

    モデルのテストを記述

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,4 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
+  it 'contentが入力されていて画像がアップロードされていれば投稿を保存できる' do
+    user = User.create!({
+      account_name: 'test',
+      email: 'test@example.com',
+      password: 'password'
+    })
+    post = user.posts.build({
+      content: Faker::Lorem.characters(number: 50)
+    })
+
+    post.images.attach(
+      {
+        io: File.open(Rails.root.join('spec/fixtures/files/test.jpeg')),
+        filename: 'test.jpeg',
+        content_type: 'image/jpeg'
+      },
+      {
+        io: File.open(Rails.root.join('spec/fixtures/files/test2.jpeg')),
+        filename: 'test2.jpeg',
+        content_type: 'image/jpeg'
+      }
+    )
+
+    expect(post).to be_valid
+  end
 end
