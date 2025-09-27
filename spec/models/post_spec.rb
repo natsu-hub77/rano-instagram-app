119c3c48c5ffb6a9841a7b27a282681f4941d456
らの <>
2025-09-28 08:37:42 +0900

    contextの記述を追加

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,29 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  it 'contentが入力されていて画像がアップロードされていれば投稿を保存できる' do
-    user = User.create!({
+  context 'contentが入力されていて画像がアップロードされていれば' do
+    before do
+      user = User.create!({
       account_name: 'test',
       email: 'test@example.com',
       password: 'password'
-    })
-    post = user.posts.build({
-      content: Faker::Lorem.characters(number: 50)
-    })
+      })
+      @post = user.posts.build({
+        content: Faker::Lorem.characters(number: 50)
+      })
 
-    post.images.attach(
-      {
-        io: File.open(Rails.root.join('spec/fixtures/files/test.jpeg')),
-        filename: 'test.jpeg',
-        content_type: 'image/jpeg'
-      },
-      {
-        io: File.open(Rails.root.join('spec/fixtures/files/test2.jpeg')),
-        filename: 'test2.jpeg',
-        content_type: 'image/jpeg'
-      }
-    )
+      @post.images.attach(
+        {
+          io: File.open(Rails.root.join('spec/fixtures/files/test.jpeg')),
+          filename: 'test.jpeg',
+          content_type: 'image/jpeg'
+        },
+        {
+          io: File.open(Rails.root.join('spec/fixtures/files/test2.jpeg')),
+          filename: 'test2.jpeg',
+          content_type: 'image/jpeg'
+        }
+      )
+    end
+    it '投稿を保存できる' do
 
-    expect(post).to be_valid
+    expect(@post).to be_valid
+  end
   end
 end
