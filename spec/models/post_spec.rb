332e59f24d5269d03f2a4179a51b57677efda0ec
らの <>
2025-09-28 14:32:01 +0900

    letの記述に変更

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -2,17 +2,20 @@ require 'rails_helper'
 
 RSpec.describe Post, type: :model do
   context 'contentが入力されていて画像がアップロードされていれば' do
-    before do
+    let!(:user) do 
       user = User.create!({
-      account_name: 'test',
-      email: 'test@example.com',
-      password: 'password'
+        account_name: 'test',
+        email: 'test@example.com',
+        password: 'password'
       })
-      @post = user.posts.build({
+    end
+
+    let!(:post) do
+      post = user.posts.build({
         content: Faker::Lorem.characters(number: 50)
       })
 
-      @post.images.attach(
+      post.images.attach(
         {
           io: File.open(Rails.root.join('spec/fixtures/files/test.jpeg')),
           filename: 'test.jpeg',
@@ -24,10 +27,35 @@ RSpec.describe Post, type: :model do
           content_type: 'image/jpeg'
         }
       )
+    post
+    
     end
-    it '投稿を保存できる' do
+    # before do
+    #   user = User.create!({
+    #   account_name: 'test',
+    #   email: 'test@example.com',
+    #   password: 'password'
+    #   })
+    #   @post = user.posts.build({
+    #     content: Faker::Lorem.characters(number: 50)
+    #   })
 
-    expect(@post).to be_valid
-  end
+    #   @post.images.attach(
+    #     {
+    #       io: File.open(Rails.root.join('spec/fixtures/files/test.jpeg')),
+    #       filename: 'test.jpeg',
+    #       content_type: 'image/jpeg'
+    #     },
+    #     {
+    #       io: File.open(Rails.root.join('spec/fixtures/files/test2.jpeg')),
+    #       filename: 'test2.jpeg',
+    #       content_type: 'image/jpeg'
+    #     }
+    #   )
+    # end
+
+    it '投稿を保存できる' do
+      expect(post).to be_valid
+    end
   end
 end
