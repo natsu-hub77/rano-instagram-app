0f6deff9480bd4274f3dda093249af4a01dfdf75
らの <>
2025-09-28 14:47:53 +0900

    保存に失敗する場合のテストを追加

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,15 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  context 'contentが入力されていて画像がアップロードされていれば' do
-    let!(:user) do 
-      user = User.create!({
-        account_name: 'test',
-        email: 'test@example.com',
-        password: 'password'
-      })
-    end
 
+  let!(:user) do 
+    user = User.create!({
+    account_name: 'test',
+    email: 'test@example.com',
+    password: 'password'
+    })
+  end
+
+  context 'contentが入力されていて画像がアップロードされていれば' do
     let!(:post) do
       post = user.posts.build({
         content: Faker::Lorem.characters(number: 50)
@@ -28,7 +29,7 @@ RSpec.describe Post, type: :model do
         }
       )
     post
-    
+
     end
     # before do
     #   user = User.create!({
@@ -56,6 +57,32 @@ RSpec.describe Post, type: :model do
 
     it '投稿を保存できる' do
       expect(post).to be_valid
+    end
+  end
+
+  context 'タイトルが未入力の場合' do
+    let!(:post) do
+      post = user.posts.create({
+        content: ''
+      })
+
+      post.images.attach(
+        {
+          io: File.open(Rails.root.join('spec/fixtures/files/test.jpeg')),
+          filename: 'test.jpeg',
+          content_type: 'image/jpeg'
+        },
+        {
+          io: File.open(Rails.root.join('spec/fixtures/files/test2.jpeg')),
+          filename: 'test2.jpeg',
+          content_type: 'image/jpeg'
+        }
+      )
+      post
+    end
+
+    it '投稿を保存できない' do
+      expect(post.errors.messages[:content][0]).to eq('を入力してください')
     end
   end
 end
