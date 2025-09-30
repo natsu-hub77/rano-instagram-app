6027f11fc4c5c2d49b3458459dc0e2a51e83b243
らの <>
2025-10-01 08:28:52 +0900

    post#indexのテストを追加

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,9 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
-  describe 'GET /posts' do
+  let!(:user) { create(:user) }
+  let!(:posts) { create_list(:post, 3, :with_images, user: user) }
+
+  before do
+    sign_in user
+  end
+
+  describe 'GET /' do
     it '200ステータスが返ってくる' do
-      get posts_path
+      get root_path
       expect(response).to have_http_status(200)
     end
   end
