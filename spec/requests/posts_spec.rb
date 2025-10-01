92ad6e5500b9f1f031ebbb9238c887ccf2704826
らの <>
2025-10-02 08:27:18 +0900

    postリクエストのテスト

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -14,4 +14,19 @@ RSpec.describe 'Posts', type: :request do
       expect(response).to have_http_status(200)
     end
   end
+
+  describe 'Post /posts' do
+    it '記事が保存される(画像必須)' do
+      post_params = attributes_for(:post)
+      file = fixture_file_upload(Rails.root.join('spec/fixtures/files/test.jpeg'), 'image/jpeg')
+
+      expect {
+        post posts_path, params: { post: post_params.merge(images: [file]) }
+      }.to change(Post, :count).by(1)
+
+      expect(response).to have_http_status(302)
+      expect(Post.last.content).to eq(post_params[:content])
+      expect(Post.last.images).to be_attached
+    end
+  end
 end
