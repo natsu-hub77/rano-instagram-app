8b0b1e5666e50b50c860aa51a2f1dc130e70622c
らの <>
2025-10-02 09:21:20 +0900

    ログインしていない場合のテスト

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -4,11 +4,12 @@ RSpec.describe 'Posts', type: :request do
   let!(:user) { create(:user) }
   let!(:posts) { create_list(:post, 3, :with_images, user: user) }
 
-  before do
-    sign_in user
-  end
-
   describe 'GET /' do
+
+    before do
+      sign_in user
+    end
+
     it '200ステータスが返ってくる' do
       get root_path
       expect(response).to have_http_status(200)
@@ -16,17 +17,30 @@ RSpec.describe 'Posts', type: :request do
   end
 
   describe 'Post /posts' do
-    it '記事が保存される(画像必須)' do
+    context 'ログインしている場合' do
+      before do
+        sign_in user
+      end
+  
+      it '記事が保存される(画像必須)' do
       post_params = attributes_for(:post)
       file = fixture_file_upload(Rails.root.join('spec/fixtures/files/test.jpeg'), 'image/jpeg')
-
-      expect {
-        post posts_path, params: { post: post_params.merge(images: [file]) }
-      }.to change(Post, :count).by(1)
+      post posts_path, params: { post: post_params.merge(images: [file]) }
 
       expect(response).to have_http_status(302)
       expect(Post.last.content).to eq(post_params[:content])
       expect(Post.last.images).to be_attached
+      end
+    end
+
+    context 'ログインしていない場合' do
+      it 'ログイン画面にリダイレクトされる' do
+        post_params = attributes_for(:post)
+        file = fixture_file_upload(Rails.root.join('spec/fixtures/files/test.jpeg'), 'image/jpeg')
+        post posts_path, params: { post: post_params.merge(images: [file]) }
+
+        expect(response).to redirect_to(new_user_session_path)
+      end
     end
   end
 end
