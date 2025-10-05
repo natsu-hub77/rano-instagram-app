2746f83e145ec03a9caf9d7955a09d65d8b164d4
らの <>
2025-10-05 20:00:37 +0900

    フォローのAPIテスト

---
+++ spec/requests/api/follows_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe 'Api::Follows', type: :request do
+  let!(:follower) { create(:user) } #フォローする側
+  let!(:account) { create(:user) } #フォローされる側
+
+  describe 'GET /api/follows' do
+    context 'ログインしている場合' do
+      before do
+        sign_in follower
+      end
+
+      it 'ユーザをフォローできる' do
+        post account_follows_path(account), as: :json
+
+        expect(response).to have_http_status(200)
+        body = JSON.parse(response.body)
+
+        expect(body['status']).to eq('followed')
+        expect(body['account_id'].to_i).to eq(account.id)
+        expect(account.reload.follow_count).to eq(1)
+      end
+    end
+  end
+end
