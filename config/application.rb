58d2dbc2aef0af1c7805763bf0ffdc9511669556
らの <>
2025-11-05 09:11:54 +0900

    dotenvの導入

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,11 @@ module RanoInstagramApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.2
+    Bundler.require(*Rails.groups)
+
+    if ['development', 'test'].include? ENV['RAILS_ENV']
+      Dotenv::Rails.load
+    end
 
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
