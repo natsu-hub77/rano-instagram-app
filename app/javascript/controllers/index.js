41a4d9af4f9826c085af341b4568ba3407078c3b
らの <>
2025-11-14 08:02:27 +0900

    index.jsファイルの記述をコメントアウト

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -3,5 +3,5 @@ import { Application } from "@hotwired/stimulus"
 const application = Application.start()
 
 // Import all controllers defined in the import map under controllers/*
-import controllers from "./**/*_controller.js"
-controllers.forEach((controller) => application.register(controller.name, controller.module))
+// import controllers from "./**/*_controller.js"
+// controllers.forEach((controller) => application.register(controller.name, controller.module))
